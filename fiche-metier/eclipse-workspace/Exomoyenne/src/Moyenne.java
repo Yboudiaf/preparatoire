ac53613d72fe1a7661cf590643dd636e8fc10cad
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2021-12-01 11:42:40 +0100

    pseudo en java exe1

---
+++ fiche-metier/eclipse-workspace/Exomoyenne/src/Moyenne.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Moyenne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// traduction du pseudo code en code java
+		
+		//DECLARATIONS DE VARIABLES
+		//reel nombre1
+		float nombre1;
+		//reel nombre2
+		float nombre2;
+		//reel moyenne
+		float moyenne;
+		
+		//SAISIE
+		//ecrire "donnez moi svp le premier nombre:"
+		System.out.println("donnez moi svp le premier nombre:");
+		//lire "nombre1"
+		Scanner sc = new Scanner(System.in);//cree un outil scanner
+		nombre1 = sc.nextFloat();
+		System.out.println("vous avez saisie nombre1="+nombre1);
+		
+		//ecrire "donnez moi svp le deuxieme nombre:"
+		System.out.println("donnez moi svp le deuxieme nombre:");
+		//lire "nombre2"	
+		nombre2 = sc.nextFloat();
+		System.out.println("vous avez saisie le nombre2="+nombre2);
+		//System.out.println("vous avez saisie le nombre2="+nombre2);
+		
+		//TRAITEMENT
+		//moyenne<--(nombre1+nombre2)/2
+		 moyenne = (nombre1+nombre2)/2;
+		 
+		//AFFICHAGE
+		//ecrire"la valeur moyenne est :",moyenne
+		System.out.println("la moyenne des 2 nombres est ="+moyenne);
+		 
+		
+		
+		
+		
+	}
+
+}
