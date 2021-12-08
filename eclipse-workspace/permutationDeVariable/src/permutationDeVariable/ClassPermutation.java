34dddfadafb3db081f2392c5ffbc09208ecab311
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2021-12-08 14:15:20 +0100

    ST Refonte de permutationVariable

---
+++ eclipse-workspace/permutationDeVariable/src/permutationDeVariable/ClassPermutation.java
@@ -0,0 +1,44 @@
+package permutationDeVariable;
+
+import java.util.Scanner;
+
+public class ClassPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//DECLARATIONS DE VARIABLES
+
+		//reel a
+		float a;
+		//reel b
+		float b;
+		//reel c
+		float c;
+
+		Scanner sc = new Scanner(System.in);
+
+
+		//SAISIE
+
+		//ecrire a
+		System.out.println("donnez moi la valeur de a");
+		a = sc.nextFloat();
+		//ecrire b
+		System.out.println("donnez moi la valeur de b");
+		b = sc.nextFloat();
+
+		//TRAITEMENT
+
+		c = a;
+		a = b;
+		b = c;	
+
+		// AFFICHAGE
+
+		//ecrire la valeur de a 
+		System.out.println("La valeur de a est " + a);
+		// ecrire la valeur de b 
+		System.out.println("La valeur de b est "+ b);
+	}
+
+}
