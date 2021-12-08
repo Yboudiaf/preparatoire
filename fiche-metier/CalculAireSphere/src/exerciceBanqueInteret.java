408fe87797947a1f637e7e4f364b02b5e7071e79
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2021-12-08 12:45:06 +0100

    exercice banque intérêt

---
+++ fiche-metier/CalculAireSphere/src/exerciceBanqueInteret.java
@@ -0,0 +1,59 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class exerciceBanqueInteret {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//DECLARATIONS DE VARIABLES
+		
+		
+		//reel interets
+		float interets;
+		//reel interets
+		double interetc;
+		//reel interetc
+		//reel somme
+		float somme;
+		//reel interet
+		float interet = 0.1f;
+		//reel annee
+		int nombreAnnee;
+		//scanner
+		Scanner sc = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		
+		//SAISIE
+		
+		//ecrire la somme
+		System.out.println("veuillez indiquer la somme:");
+		somme = sc.nextFloat();
+		//ecrire les interets
+		System.out.println("veuillez indiquer l'interet:");
+		interet = sc.nextFloat();
+		System.out.println("Erreur de cast interet");
+		//ecrire le nombre d'annee
+		System.out.println("veuillez indiquer le nombre d'année:");
+		nombreAnnee = sc.nextInt();
+		
+		
+		
+		//TRAITEMENT
+		
+		interets = somme*(1+nombreAnnee*interet);
+		interetc = somme*Math.pow((1+interet),nombreAnnee);
+		
+		//AFFICHAGE
+		
+		//ecrire la valeur aquise par la somme avec interet simple est 
+		System.out.println("la valeur aquise pa la somme avec interet simple est:"+df.format(interets));
+		System.out.println("la valeur aquise par la somme avec interet composé est:"+df.format(interetc));
+				
+				//
+				sc.close();
+
+	}
+
+}
