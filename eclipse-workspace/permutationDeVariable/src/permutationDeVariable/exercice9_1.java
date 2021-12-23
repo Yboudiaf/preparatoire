aa07413123df6122a13f87c8039c531da58b02ba
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2021-12-23 15:02:56 +0100

    create exo 9-1

---
+++ eclipse-workspace/permutationDeVariable/src/permutationDeVariable/exercice9_1.java
@@ -0,0 +1,48 @@
+package permutationDeVariable;
+
+import java.util.Scanner;
+
+public class exercice9_1 {
+
+	public static void main(String[] args) {
+
+		//DECLARATIONS DE VARIABLES
+
+		//ENTIER A
+		int A;
+		//ENTIER B
+		int B;
+
+		//SAISIE ET TRAITEMENT
+
+		//ECRIRE SAISISSEZ LA VALEUR A
+		System.out.println("Saisissez la valeur A");
+		//OUTIL SCANNER
+		Scanner sc = new Scanner(System.in);
+		//LIRE A
+		A = sc.nextInt();
+		//ECRIRE SAISISSEZ LA VALEUR B
+		System.out.println("Saisissez la valeur B");
+		//LIRE B
+		B = sc.nextInt();
+		//SI A<B alors
+		if(A<B) {
+			//AFFICHER A<B
+			System.out.println("A<B");
+		}	
+			//SINON SI A>B
+		else if(A>B){
+			//AFFICHER A>B
+			System.out.println("A>B");
+		
+		}
+		//SINON
+		else{
+			//AFFICHER A=B
+			System.out.println("A=B");
+
+		}
+	}
+}
+
+
