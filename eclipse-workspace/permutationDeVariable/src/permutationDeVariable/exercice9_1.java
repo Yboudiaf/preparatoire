0048d6033c88c74ba0342a8612ff0203f178058c
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2022-01-06 12:51:06 +0100

    les boucles for

--- eclipse-workspace/permutationDeVariable/src/permutationDeVariable/exercice9_1.java
+++ eclipse-workspace/permutationDeVariable/src/permutationDeVariable/exercice9_1.java
@@ -9,37 +9,37 @@ public class exercice9_1 {
 		//DECLARATIONS DE VARIABLES
 
 		//ENTIER A
-		int A;
+		int a;
 		//ENTIER B
-		int B;
+		int b;
 
 		//SAISIE ET TRAITEMENT
 		Scanner sc = new Scanner(System.in);
 		//ECRIRE SAISISSEZ LA VALEUR A
-		System.out.println("Saisissez la valeur A");
+		System.out.println("Saisissez la valeur a");
 		//OUTIL SCANNER
-		
+
 		//LIRE A
-		A = sc.nextInt();
+		a = sc.nextInt();
 		//ECRIRE SAISISSEZ LA VALEUR B
-		System.out.println("Saisissez la valeur B");
+		System.out.println("Saisissez la valeur b");
 		//LIRE B
-		B = sc.nextInt();
+		b = sc.nextInt();
 		//SI A<B alors
-		if(A<B) {
+		if(a<b) {
 			//AFFICHER A<B
-			System.out.println("A<B");
+			System.out.println("a<b");
 		}	
-			//SINON SI A>B
-		else if(A>B){
+		//SINON SI A>B
+		else if(a>b){
 			//AFFICHER A>B
-			System.out.println("A>B");
-		
+			System.out.println("a>b");
+
 		}
 		//SINON
 		else{
 			//AFFICHER A=B
-			System.out.println("A=B");
+			System.out.println("a=b");
 
 		}
 	}
---
+++ eclipse-workspace/permutationDeVariable/src/permutationDeVariable/les_boucles.java
@@ -0,0 +1,15 @@
+package permutationDeVariable;
+
+public class les_boucles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a;
+		
+		for(a = 1; a <= 6 ; a++ ) {
+			System.out.println("claquer dans les mains");
+		}
+	}
+
+}
