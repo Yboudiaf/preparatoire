58d366eeadddf6d0e8a76c5157c4aec4f52d50b1
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2022-01-17 10:53:45 +0100

    exercice la valeur absolue

--- eclipse-workspace/permutationDeVariable/src/permutationDeVariable/nombreEntierDiviseur.java
+++ eclipse-workspace/permutationDeVariable/src/permutationDeVariable/nombreEntierDiviseur.java
@@ -5,28 +5,20 @@ import java.util.Scanner;
 public class nombreEntierDiviseur {
 
 	public static void main(String[] args) {
-		
-		
-			int n ;
-			
-			System.out.println("veuillez saisir un nombre entier");
-			Scanner sc= new Scanner(System.in);
-			n = sc.nextInt().toLowerCase;
-			if(n == n || n == 1) {
-				
-				System.out.println("la valeur saisie n'est pas autorisée veuillez saisir un nombre diferent de 0 et : "+ n ");
-			}
-			else {
-								
-			System.out.println(n );
-				
-			}
-				
-				
-				
-			
-			while(n );
-			}
-	}
 
+
+		int n ;
+
+		System.out.println("veuillez saisir un nombre entier");
+		Scanner sc= new Scanner(System.in);
+		n = sc.nextInt();
+		if(n < 0 ) {
+			n = -n;
+			
+		}
+		System.out.println("La valeur absolue est "+ n);
+
+		
+	}
+		
 }
