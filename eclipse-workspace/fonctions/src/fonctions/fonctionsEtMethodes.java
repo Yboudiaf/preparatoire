70e57994a97a00d9300089bbcf34eb871405a50c
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2022-01-31 17:02:47 +0100

    fonction methode

---
+++ eclipse-workspace/fonctions/src/fonctions/fonctionsEtMethodes.java
@@ -0,0 +1,34 @@
+package fonctions;
+
+public class fonctionsEtMethodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int ageATester =19;	
+		
+		String resultat = majeurOuMineur(ageATester);
+		System.out.println(resultat);
+		
+	}//fin de main
+	
+	public static String majeurOuMineur(int ageTester){
+		
+		if(ageTester <18) {
+			
+			return "vous êtes mineur";
+		}
+		else {
+			
+			return "vous etes majeur";
+		}
+		
+	}
+	
+	
+	
+	
+	
+}//fin de classe
+
+
