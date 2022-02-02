abe828bf8d6e64ece687135529e030c5c4e2dec4
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2022-02-02 10:23:30 +0100

    conversion chaine de caractère en un nombre entier

--- eclipse-workspace/conversion/src/conversion/conversionKmMiles.java
+++ eclipse-workspace/conversion/src/conversion/conversionKmMiles.java
@@ -6,32 +6,38 @@ public class conversionKmMiles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double nombre;	
-		int km = 1000000;
+		float nombre;	
+		float km = 1000000;
 		double miles =1.609 ;
-		String q = null ;
+		String saisie = null ;
 		long horslimite= 1000001;
+
 		// (km < 0.01 || km > 1000000)
 		Scanner sc = new Scanner(System.in);
 		System.out.println("veuillez saisir une valeur en km");
-		nombre =sc.nextInt();
-		System.out.print("La valeur de " + nombre + " en km est: ");
-	    miles = nombre/1.609;
-		System.out.print(miles + " miles");
-		boolean macondition =(horslimite < km);
-		if(macondition == true) {
-			System.out.println(" la saisie est: hors limite" );	
-			
+			saisie =sc.nextLine();
+			if(!saisie.equals("q")) {
 				
 			}
-		else if(q.equals("q")){
-			
-			System.out.println("programme terminer");
-			
-		}sc.close();
+			//
+		System.out.print("La valeur de " + nombre + " en km est: ");
+			miles = nombre/1.609;
+		System.out.print(miles + " miles");
+
+		boolean macondition =(horslimite < km);
+			if(macondition == true) {
+			System.out.println(" la saisie est: hors limite" );	
+
+
 		}
-		
+		else if(q.equals(nombre)){
+		//	 nombre =  q ; 
+			System.out.println("programme terminer");
+
+		}sc.close();
 	}
-	
+
+}
+
 
 
