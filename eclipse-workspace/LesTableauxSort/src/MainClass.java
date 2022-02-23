6cdee4e5d760cb93438209399f80be75ee69e7b8
Yboudiaf <92739831+Yboudiaf@users.noreply.github.com>
2022-02-23 16:51:40 +0100

    calculer le nombre de jeunes

--- eclipse-workspace/LesTableauxSort/src/MainClass.java
+++ eclipse-workspace/LesTableauxSort/src/MainClass.java
@@ -1,23 +1,41 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
 
+import java.util.Scanner;
+/*Exercice 2.7.1 : Calculer le nombre de jeunes
+//Il s'agit de dénombrer toutes les personnes d'âge inférieur strictement à 20 ans parmi un échantillon de 20 
+//personnes. L’utilisateur est invité à saisir les 20 valeurs.
+//Décrivez l'algorithme qui affiche le nombre de jeunes et codez la solution.*/
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int nbJeunes = 0;
-		int[] tableau = new int[20];
+		int nbVieux = 0;
+		int nbVingt=20;
+		int[] tableau = new int[5];
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Veuillez entrez 20 nombres");
 		for (int compteur = 0; compteur < tableau.length; compteur++) {
 			tableau[compteur] = sc.nextInt();
-			if (tableau[compteur] < 20) {
+		 if(tableau[compteur] < 20) {
 				nbJeunes++;
+			}else if(tableau[compteur] > 20){
+				nbVieux++;
 			}
 		}
-		System.out.println("il y'a " + nbJeunes + " personnes de - de 20 ans");
-		Arrays.sort(tableau);
+		
+		if(nbVieux == tableau.length) {
+			System.out.println("TOUTES LES PERSONNES ONT PLUS DE 20 ANS ");
+		}
+		else if(nbJeunes==tableau.length) {
+			System.out.println("TOUTES LES PERSONNES ONT MOINS DE 20 ANS");
+		
+		}
+		else if((nbVieux == nbVingt)&&(nbJeunes == nbVingt)) {
+			System.out.println("TOUTES LES PERSONNES ONT 20 ANS");
+		}
+		System.out.println("il y'a " + nbJeunes + " personnes de - de 20 ans et "+nbVieux+" personnes de + de 20 ans");
+		
+		//Arrays.sort(tableau);
 		sc.close();
 	}
 
